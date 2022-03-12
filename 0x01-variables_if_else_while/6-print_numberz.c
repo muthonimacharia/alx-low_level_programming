8fcb0a7cee6c82aee975e0821ef37a3579e05c69
Muthoni_Macharia <paullymuthoni@gmail.com>
2022-03-12 09:59:04 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,8 +3,7 @@
 /**
 *main - prints all single digit numbers of base 10 starting from 0
 *followed by a new line, using putchar
-*Return: Always 0 
-(Sucess0
+*Return: Always 0 (Sucess)
 */
 int main(void)
 {
