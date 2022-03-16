2f5e2ca3b2b6aeab6c22fed7f3443713798258b7
Muthoni_Macharia <paullymuthoni@gmail.com>
2022-03-16 22:30:49 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- *print_alphabet - print alphabet
- *
- *Return: void
- *
- */
-=======
 * print_alphabet - print alphabet
 *
 * Return: 0
 *
-*/
->>>>>>> 601f3090295965cad1a3fc575c9448428c496ff3
+
 void print_alphabet(void)
 {
 	char letter = 'a';
