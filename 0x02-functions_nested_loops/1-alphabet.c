60fc391cbe0699f97d0346eda3afba612e56f260
muthonimacharia <paullymuthoni@gmail.com>
2022-03-16 21:33:17 +0300

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,58 +1,27 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 #include "main.h"
 
 /**
 *print_alphabet - print alphabet
 *
-*Return: void
-*/
-void print_alphabet(void)
-{
-	
-	char letter = 'a';
-	
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
-}
-=======
-#include "main.h"
 
-/**
-*print_alphabet - print alphabet
-*
+
+
+
+
+
+
+
+
+
+
 *Return: void
 *
 */
 void print_alphabet(void)
 {
 	char letter = 'a';
-	
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
-}
->>>>>>> dd411685fd7b3039370ff9dd17f2d48da1998f24
-=======
-#include "main.h"
 
-/**
- *print_alphabet - print alphabet
- *
- *Return: void
- *
- */
-void print_alphabet(void)
-{
-	char letter = 'a';
-	
 	while (letter <= 'z')
 	{
 		_putchar(letter);
@@ -60,4 +29,3 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
->>>>>>> d80dffa3aa83bc4e91f7e75dd1223b9147659625
