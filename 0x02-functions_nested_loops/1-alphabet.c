ccf0d4438469bb20aa99b697049b103ba31c3017
Muthoni_Macharia <paullymuthoni@gmail.com>
2022-03-16 22:03:10 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-
 #include "main.h"
 
 /**
 *print_alphabet - print alphabet
 *
-
-
-
-
-
-
-
-
-
-
-
 *Return: void
 *
 */
@@ -30,24 +17,3 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
-=======
-#include "main.h"
-
-/**
-*print_alphabet - print alphabet
-*
-*Return: void
-*
-*/
-void print_alphabet(void)
-{
-	char letter = 'a';
-	
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
-}
->>>>>>> a8c7a85f5dfe14b84ab655e50aeac036bc414ee6
