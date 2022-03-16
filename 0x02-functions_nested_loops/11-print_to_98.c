bf865a7483b49fc9d83861b686049c3e2a233af5
Muthoni_Macharia <paullymuthoni@gmail.com>
2022-03-16 23:58:02 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,10 +23,10 @@ printf("%d\n", i);
 }
 else if (n >= 98)
 {
-for (j = n; j >= 98; j)
+for (j = n; j >= 98; j--)
 {
 if (j != 98)
-printf("%d, ", j--);
+printf("%d, ", j);
 else if (j == 98)
 printf("%d\n", j);
 }
