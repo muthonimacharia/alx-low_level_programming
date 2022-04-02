82cf8e2b82e9a40905d7fda3560f256558ecf31c
muthonimacharia <paullymuthoni@gmail.com>
2022-04-02 03:03:55 +0300

    mul

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -30,3 +30,22 @@ int main(int argc, char *argv[])
 		printf("%s\n", argv[i]);
 	return (0);
 }
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
